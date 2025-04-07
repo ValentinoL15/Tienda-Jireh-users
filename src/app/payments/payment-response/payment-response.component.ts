4146318ea65128e795757ba48aab1276f1b17a39
ValentinoL15 <pichilongo1@gmail.com>
2025-04-07 10:18:03 -0300

    create payment-response

---
+++ src/app/payments/payment-response/payment-response.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-response',
+  imports: [],
+  templateUrl: './payment-response.component.html',
+  styleUrl: './payment-response.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PaymentResponseComponent {
+
+}
