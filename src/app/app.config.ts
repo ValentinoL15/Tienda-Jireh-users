d9ed5d27b0d1a6ca96f59c448447170829b50c28
ValentinoL15 <pichilongo1@gmail.com>
2025-04-07 14:28:07 -0300

    up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
@@ -13,7 +13,7 @@ import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding()), provideHttpClient(withFetch(), withInterceptors([SpinnerInterceptor])),
+    provideRouter(routes,withHashLocation() ,withComponentInputBinding()), provideHttpClient(withFetch(), withInterceptors([SpinnerInterceptor])),
     provideClientHydration(withEventReplay()),
     provideAnimations(),
     provideToastr(),
