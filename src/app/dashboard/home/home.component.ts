ee7d0ff9c6a43cb9aa13ac2819c1ae06d6b521ed
ValentinoL15 <pichilongo1@gmail.com>
2025-04-15 11:45:59 -0300

    update

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,16 +1,18 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
-import { Tag } from 'primeng/tag';
 import { Product } from '../../interfaces/interfaces';
 import { ProductsService } from '../services/products.service';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 import { ToastrService } from 'ngx-toastr';
+import { Carousel } from 'primeng/carousel';
+
+
 
 
 @Component({
   selector: 'app-home',
-  imports: [Tag, ButtonModule, PaginatorModule,RouterLink],
+  imports: [ButtonModule, PaginatorModule,RouterLink,Carousel],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -25,15 +27,41 @@ export class HomeComponent implements OnInit{
   skip: number = 0;
   limit: number = 12;
   first: number = 0;
+  
+  responsiveOptions: any[] | undefined;
 
   ngOnInit(): void {
   this.getAllProducts()
+  this.responsiveOptions = [
+    {
+        breakpoint: '1400px',
+        numVisible: 2,
+        numScroll: 1,
+    },
+    {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1,
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 2,
+        numScroll: 1,
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1,
+        numScroll: 1,
+    },
+];
   
   }
 
   get filteredProducts() {
-    return this.products.filter(p => p.shoes?.length > 0);
+    return this.products.filter(p => p.shoes);
   }
+
+  
 
 getAllProducts(skip: number = 0, limit: number = this.limit) {
   this.productServ.getAllProducts(skip, limit).subscribe({
